cfd02110620ea42b989266d7dd92fe81a6f6443f
jairoxzn <jairolajo4@gmail.com>
2026-06-06 18:59:11 -0500

    Fix: agregar scripts locales de JS y correccion del componente Icon

--- server.js
+++ server.js
@@ -90,6 +90,14 @@ app.use(cors({ origin: '*', methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 app.use(express.json());
 app.use(morgan('dev'));
 
+// Desactivar caché para desarrollo y asegurar que siempre se sirva el index.html más reciente
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  next();
+});
+
 // Servir la carpeta estática "public" para el frontend SPA
 app.use(express.static(path.join(__dirname, 'public')));
 
